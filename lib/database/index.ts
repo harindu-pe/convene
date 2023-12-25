23673d1c361f9f85568876105d54d63451dab388
harindu-pe <harindu.pe@gmail.com>
2023-12-25 16:32:36 +0530

    second

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
-const MONOGODB_URI = process.env.MONOGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONOGODB_URI) throw new Error("MONOGDB_URI is missing");
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
   cached.promise =
     cached.promise ||
-    mongoose.connect(MONOGODB_URI, {
+    mongoose.connect(MONGODB_URI, {
       dbName: "convene",
       bufferCommands: false,
     });
